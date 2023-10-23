a0333971291ba7bd77072612aefa33cdceff5cf4
davidgfb <davidgracia99@gmail.com>
2023-10-23 20:24:07 +0200

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -16,26 +16,42 @@ TODO: Hay problema con el rango de valores. Entrada: 0...1 check,
 pesos: -7...7? Bias: 4...9? salida: 0, 1, imgPesos: -6...6?
 creo q no hay problema en q el rango de la img pesos sea [-2...2]
 [-2*255...2*255]
-'''
+cuela un cuadrado al final y ajusta bias
 
 m_Fila_Img_Ent, m_Fila_Img_Pesos = (array(imread(ruta)).flatten()
     for ruta in ('ml/circunferencia/c_b&n.png', 'pesos.png'))
+'''
 
-for i in range(2):
+m_Fila_Img_Pesos = array(imread('pesos.png')).flatten()
+
+rutas = (*('ml/circunferencia/c' + str(i) + '_b&n.png'
+         for i in range(5)),)
+
+#print(rutas)
+
+def check():
+    pass
+
+for ruta in rutas:
+    m_Fila_Img_Ent = array(imread(ruta)).flatten()
+    
     salida = m_Fila_Img_Ent @ m_Fila_Img_Pesos
+
+    print(salida)
 
     '''
     #print(salida) #298895.5
     #print(not(salida > bias == es_Circunferencia), True)
     #print(not(True, False)) #wtf!?
 
-    la salida es distinta a/no coincide con la realidad. 
+    la salida es distinta a/no coincide con la realidad.
+    todavia no exporta png pesos
     '''
     if salida <= bias: # = es_Circunferencia:
         m_Fila_Img_Pesos += m_Fila_Img_Ent #-=
         
         exportacion = reshape(m_Fila_Img_Pesos,
-            open('ml/circunferencia/c_b&n.png').size[::-1])
+            open(ruta).size[::-1])
 
         '''
         imshow(exportacion)
