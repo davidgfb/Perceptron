d3e59d2b944169610152327d4864ba56ade1204b
davidgfb <davidgracia99@gmail.com>
2023-11-27 01:07:07 +0100

    Create Main1.py

---
+++ Main1.py
@@ -0,0 +1,20 @@
+#https://neptune.ai/blog/backpropagation-algorithm-in-neural-networks-guide
+from math import exp
+from numpy import array, round
+
+a, w, b, y, eps, sig_r, r = 0.1, 0.5, 1.83, 0.03, 6, 0, int(1e3)
+#eps valor dorado #sig rdada
+ 
+for i in range(r):    
+    sig = 1/(1+exp(-(a*w+b)))
+    y_msig = y-sig
+    upd = eps * y_msig
+    b += upd  
+    w += a * (upd + eps * y_msig * sig * (1 - sig))
+
+    sig_r = round(sig,2)
+    if sig_r == y: break
+                
+print('eps:', eps, 'N:', i, 'sig:', sig_r, 'debe ser eps: 6 N: 4 sig: 0.03') 
+
+#TODO: q el programa determine el valor dorado
